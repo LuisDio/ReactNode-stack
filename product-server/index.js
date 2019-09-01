0cfd3759f42a578f8c481418343804a689883bc8
Luis <luis.diolindo@gmail.com>
2019-09-01 15:55:31 +0300

    add insert to database

--- product-server/index.js
+++ product-server/index.js
@@ -10,7 +10,6 @@ const SELECT_ALL_PRODUCTS_QUERY = 'SELECT * FROM products';
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    //password: 'theo',
     database: 'react_sql'
 });
 
@@ -25,6 +24,18 @@ app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('go to products to see products');
+});
+
+app.get('/products/add', (req, res) => {
+    const { name, price} = req.query;
+    const INSERT_PRODUCTS_QUERY = `INSERT INTO products(name, price) VALUES('${name}', ${price})`;
+    connection.query(INSERT_PRODUCTS_QUERY, (err, results) => {
+        if(err) {
+            return res.send(err);
+        } else {
+            return res.send('successfully added products');
+        }
+    })
 });
 
 app.get('/products', (req, res) => {
