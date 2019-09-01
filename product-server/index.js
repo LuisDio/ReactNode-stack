30e6ba3bc724e140084de0287d4cfebf6b274c00
Luis <luis.diolindo@gmail.com>
2019-09-01 15:31:22 +0300

    query table from

--- product-server/index.js
+++ product-server/index.js
@@ -1,10 +1,45 @@
 const express = require('express');
 const cors = require('cors');
+const mysql = require('mysql');
 
 const app = express();
 
+const SELECT_ALL_PRODUCTS_QUERY = 'SELECT * FROM products';
+
+// This give us an instance our connection
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    //password: 'theo',
+    database: 'react_sql'
+});
+
+connection.connect(err => {
+    if(err) {
+        return err;
+    }
+});
+
+
 app.use(cors());
 
+app.get('/', (req, res) => {
+    res.send('go to products to see products');
+});
+
+app.get('/products', (req, res) => {
+    connection.query(SELECT_ALL_PRODUCTS_QUERY, (err, results) => {
+        if (err) {
+            return res.send(err);
+        } else {
+            return res.json({
+                data: results
+            })
+        }
+
+    });
+});
+
 app.listen(4000, () => {
-    console.log(`products server listening on port 4000`);
+    console.log(`products server listening on the port 4000`);
 })
